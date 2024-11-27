283c899f47904c20784cbd4ad555c139a10b9d1e
minseok <alstjrwjd99@naver.com>
2024-11-27 19:52:51 +0900

    feat(view): Show info about input and output

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import CustomView from "./view/inputView.js";
+
 class App {
   async run() {
-    
+    const userInput = await CustomView.getStringForAdd();
+    CustomView.printResult(userInput);
   }
 }
 
---
+++ src/view/inputView.js
@@ -0,0 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class CustomView {
+    static async getStringForAdd() {
+        return await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    }
+
+    static async printResult(sumValue) {
+        await MissionUtils.Console.print(`결과 : ${sumValue}`);
+    }
+}
+
+export default CustomView;
